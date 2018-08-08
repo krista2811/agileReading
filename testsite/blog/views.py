435e7bb3c8ea55b90d7db0e1eec1af1b22d33d5f
Mephisto405 <ciy405x@kaist.ac.kr>
2018-08-08 10:57:44 +0900

    [#9][#8] improve signing up feature

--- testsite/blog/views.py
+++ testsite/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
-from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
 
 # Create your views here.
 
@@ -15,11 +15,11 @@ def signup(request):
     서버는 UserCreationForm 으로 부터 각 내용들을 받아서 처리한다
     """
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('login')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     return render(request, 'registration/signup.html', {'form': form})
 
